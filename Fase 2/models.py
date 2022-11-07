bd2f9c9ff947348ce63fff54f3fd2f0928f7fb88
LuizaValezim <luizavalezim@gmail.com>
2022-11-07 00:03:30 -0300

    Adicionando parte final Projeto 1

---
+++ Fase 2/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Inventario(Base):
+    __tablename__ = "inventario"
+
+    id_produto = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nome = Column(String(300), index=True)
+    quantidade = Column(Integer, primary_key = True, index=True, default=0)
+    preco = Column(Float)
+
+    movimentacao = relationship("Movimentacao", back_populates="inventario", cascade="all, delete")
+    
+class Movimentacao(Base):
+    __tablename__ = "movimentacao"
+    id_movimentacao = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    id_produto = Column(Integer, ForeignKey("inventario.id_produto"))
+    quantidade = Column(Integer)
+
+    inventario = relationship("Inventario", back_populates="movimentacao")
---
+++ Fase 2/schemas.py
@@ -0,0 +1,29 @@
+from unicodedata import name
+from pydantic import BaseModel
+from pydantic import Field
+
+class Produto(BaseModel):
+    id_produto: int 
+    nome: str
+    preco: 7.5
+
+class Cria_Inventario(Produto):
+    pass
+
+class Inventario(Produto):
+    quantidade : int = Field(default = 0)
+    class Config:
+        orm_mode = True
+
+class Movimentacao(BaseModel):
+    id_movimentacao: int = Field(default=None)
+    quantidade: int
+
+class Cria_Movimentacao(Movimentacao):
+    id_produto: int
+    pass
+
+class Movimentacao_Produto(Movimentacao):
+    id_produto: int
+    class Config:
+        orm_mode = True
